e0adc4d7bb92a303dbfc0e336927caabf776970d
Andy Lu <alu@futuredial.com>
2022-05-22 14:23:33 +0800

    refactor

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -7,41 +7,18 @@ public interface IAuthentication
 
 public class AuthenticationService : IAuthentication
 {
-    // private readonly IFailedCounter _failedCounter;
     private readonly IHash _hash;
-
-    // private readonly ILog _log;
-
-    // private readonly INotification _notification;
     private readonly IOtp _otp;
-
     private readonly IProfile _profile;
-    private readonly LogDecorator _logDecorator;
 
-    // private readonly FailedCounterDecorator _failedCounterDecorator;
-    // private readonly NotificationDecorator _notificationDecorator;
-
-    public AuthenticationService(IHash hash, ILog log, IOtp otp, IProfile profile
+    public AuthenticationService(IHash hash, IOtp otp, IProfile profile
     )
     {
         _hash = hash;
-        // _log = log;
         _otp = otp;
         _profile = profile;
-        // _logDecorator = new LogDecorator(this);
     }
-
-    // public AuthenticationService()
-    // {
-    //     _profile = new ProfileDao();
-    //     _hash = new Sha256Adapter();
-    //     _otp = new Otp();
-    //     // _notification = new SlackAdapter();
-    //     _failedCounter = new FailedCounter();
-    //     _log = new NLogAdapter();
-    //     new NotificationDecorator(this, new SlackAdapter());
-    // }
-
+    
     public bool Verify(string accountId, string password, string otp)
     {
         var passwordFromDb = _profile.GetPasswordFromDb(accountId);
@@ -49,10 +26,6 @@ public class AuthenticationService : IAuthentication
         var otpFromApi = _otp.GetCurrentOtp(accountId);
 
         return passwordFromDb == inputPassword && otp == otpFromApi;
-
-        // _logDecorator.LogFailedCount(accountId);
-
-        // _notificationDecorator.NotifyWhenNotify();
     }
 }
 
