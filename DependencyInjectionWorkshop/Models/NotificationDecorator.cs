b6a5ec8ef8b27a029822c750c33afd475279936a
Andy Lu <alu@futuredial.com>
2022-05-22 13:26:05 +0800

    refactor: NotificationDecorator.cs

--- DependencyInjectionWorkshop/Models/NotificationDecorator.cs
+++ DependencyInjectionWorkshop/Models/NotificationDecorator.cs
@@ -2,7 +2,7 @@ namespace DependencyInjectionWorkshop.Models;
 
 public class NotificationDecorator : IAuthentication
 {
-    private IAuthentication _authenticationService;
+    private readonly IAuthentication _authenticationService;
     private readonly INotification _notification;
 
     public NotificationDecorator(IAuthentication authenticationService, INotification notification)
@@ -11,19 +11,16 @@ public class NotificationDecorator : IAuthentication
         _notification = notification;
     }
 
-    private void NotifyWhenNotify()
-    {
-        _notification.Notify("Login failure");
-    }
-
     public bool Verify(string accountId, string password, string otp)
     {
         var verify = _authenticationService.Verify(accountId, password, otp);
-        if (!verify)
-        {
-            NotifyWhenNotify();
-        }
+        if (!verify) NotifyWhenNotify();
 
         return verify;
+    }
+
+    private void NotifyWhenNotify()
+    {
+        _notification.Notify("Login failure");
     }
 }
